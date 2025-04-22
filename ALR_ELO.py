c24b412f90def435d677721bb0df5c1b5d4166d9
Simon Persson <simon2@kth.se>
2025-04-22 11:26:31 +0200

    Added handling for missing folders/files and added overwrite protection

--- ALR_ELO.py
+++ ALR_ELO.py
@@ -1,5 +1,18 @@
 import csv
 import glob
+from pathlib import Path
+import re
+
+def challengeYesOrNo(question="Continue?"):
+    # Inspired by https://stackoverflow.com/a/3041990
+    valid = {"y": True, "yes": True, "n": False, "no": False}
+
+    while True:
+        choice = input(f"{question} [y/n] ").lower()
+        if choice in valid:
+            return valid[choice]
+        else:
+            print("Please respond with y or n")
 
 def importResults(file):
     file = file
@@ -42,10 +55,10 @@ def sortStages(files, round):
             stageTime = driver[4]
 
             if displayName == "WRC Player":
-                if wrcPlayers and len(wrcPlayers) == 1: #NEEDS TO BE FIXED
+                if idy != 0 and len(wrcPlayers) == 1:
                     displayName = wrcPlayers[0]
                 else:    
-                    displayName = input("Enter the name of the driver in position " + position + " of stage " + str(idy+1) + ": ")
+                    displayName = input(f"Enter the name of the driver in position {position} of stage {str(idy+1)}: ")
                     wrcPlayers.append(displayName)
 
             if stageTime in nominalTimes:
@@ -69,13 +82,56 @@ def sortStages(files, round):
         results[stage] = stageResult
     return results
 
-if __name__ == "__main__":
-    club = input("Enter the name of the club (WRC1 / WRC2 / WREC / SC): ").upper()
-    round = input("Enter the season and round as 'S* R*': ").upper()
-    path = club + "/" + round
+def main():
+    clubs = ("WRC1", "WRC2", "WREC")
+    
+    for c in clubs: #Create the folder structure if it doesn't exist
+        Path(c).mkdir(exist_ok=True)
+        Path("Output/" + c).mkdir(parents=True, exist_ok=True)
+    
+    while True:
+        club = input("Enter the name of the club (WRC1 / WRC2 / WREC): ").upper()
+        if club not in clubs:
+            if not challengeYesOrNo(f"{club} is not a valid club. Try again?"):
+                quit("No results have been exported")
+        else:
+            break
+    
+    while True:
+        round = input("Enter the season and round as 'S# R#': ").upper()
+        path = f"{club}/{round}"
+        if not Path(path).is_dir():
+            if not challengeYesOrNo(f"No directory for {club}/{round} exists. Try again?"):
+                quit("No results have been exported")
+        else:
+            if Path(f"Output/{club}/{round}.csv").is_file():
+                if not challengeYesOrNo(f"An output for {club} {round} already exists. Overwrite?"):
+                    quit("No results have been exported")
+            break
+    
     files = sorted(glob.glob(path + "/" + "*stage*"), key=len)
+    
     if not files:
-        print("No files found")
-    else:    
+        quit("No files were found")
+    else:
+        stagep = r"^wrc2023_event_[a-zA-Z0-9]+_stage[0-9]+_leaderboard_results.csv$"
+        overallp = r"^wrc2023_event_[a-zA-Z0-9]+_stage_overall_leaderboard_results.csv$"
+        stagenum = 0
+        overallnum = 0
+        for f in files:
+            if re.match(stagep, f.split("/")[-1]):
+                stagenum += 1
+            elif re.match(overallp, f.split("/")[-1]):
+                overallnum += 1
+        print("Found the following number of stages")
+        print(f"Stage files: {stagenum}")
+        print(f"Overall: {overallnum}")
+    
+    if challengeYesOrNo():
         exportResults(sortStages(files, round), path)
         print("ELO results exported")
+    else:
+        print("No results have been exported")
+
+if __name__ == "__main__":
+    main()
